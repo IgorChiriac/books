5745411dc3742a8df9c8084b5b5b48c771edc28a
Igor Chiriac <igor@electricfeel.com>
2021-05-28 16:19:47 +0200

    ch13

--- books/tests.py
+++ books/tests.py
@@ -2,6 +2,7 @@ from django.test import Client, TestCase
 from django.contrib.auth import get_user_model
 from django.urls import reverse
 from .models import Book, Review
+from django.contrib.auth.models import Permission
 
 class BookTests(TestCase):
   def setUp(self):
@@ -24,6 +25,14 @@ class BookTests(TestCase):
     self.assertEqual(f'{self.book.title}', 'Harry Potter') 
     self.assertEqual(f'{self.book.author}', 'JK Rowling') 
     self.assertEqual(f'{self.book.price}', '25.00')
+
+  def test_book_list_view_for_logged_in_user(self):
+    self.client.login(email='reviewuser@email.com', password='testpass123') 
+    response = self.client.get(reverse('book_list'))
+    self.assertEqual(response.status_code, 200) 
+    self.assertContains(response, 'Harry Potter') 
+    self.assertTemplateUsed(response, 'books/book_list.html')
+    
   def test_book_list_view(self):
     response = self.client.get(reverse('book_list'))
     self.assertEqual(response.status_code, 200)
