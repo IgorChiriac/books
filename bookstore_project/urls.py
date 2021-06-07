b65fa1f5388f4257f44e64f44811618bbe7f4367
Igor Chiriac <igor@electricfeel.com>
2021-06-07 14:13:31 +0200

    ch17

--- bookstore_project/urls.py
+++ bookstore_project/urls.py
@@ -19,7 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('manage-books/', admin.site.urls),
     
     # User management
     path('accounts/', include('allauth.urls')),
